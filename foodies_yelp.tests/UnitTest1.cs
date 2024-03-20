6d9b96facab71da9652547cd2c2e05d3634c0a55
Keith <kayceecode@gmail.com>
2024-03-19 22:21:26 -0400

    Added NUnit project

---
+++ foodies_yelp.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace foodies_yelp.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
