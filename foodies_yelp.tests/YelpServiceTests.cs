a16762009781e43730b1641ca10aed2066c86b5c
Keith <kayceecode@gmail.com>
2024-05-02 00:03:06 -0400

    added more valid tests and a client test.

--- foodies_yelp.tests/YelpServiceTests.cs
+++ foodies_yelp.tests/YelpServiceTests.cs
@@ -11,13 +11,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using System.Net;
 
-
 namespace foodies_yelp.testing;
 
 [TestFixture]
 public class YelpServiceTests
 {
     private List<Business> _businesses = new List<Business>(3);
+    private List<Review> _reviews = new List<Review>(3);
     private ILogger<YelpService> _mockLogger;
     private Mock<IHttpClientFactory> _mockHttpClientFactory;
     private IConfiguration _mockConfiguration;
@@ -26,9 +26,8 @@ public class YelpServiceTests
     [SetUp]
     public void Setup()
     {
-        var service = new Mock<YelpService>();
         CreateBusinesses();
-        CreateMockHttpClient();
+        var service = new Mock<YelpService>();
         _mockYelpOptions = new Mock<IOptions<Yelp>>();
         var mockSearchDto = new Mock<SearchDto>();
         var serviceProvider = new ServiceCollection()
@@ -46,17 +45,23 @@ public class YelpServiceTests
         _mockYelpOptions.Setup(m => m.Value).Returns(new Yelp { Key = "Test Key" });
     }
 
-    private void CreateMockHttpClient()
+    private void CreateMockHttpClient(YelpResponse yelpResponse = null!)
     {
         var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-
-        var yelpResponse = new YelpResponse()
-        {
-           Businesses = _businesses,
-           Reviews = new()
-        };
+        StringContent httpContent;
         
-        var httpContent = new StringContent(JsonConvert.SerializeObject(yelpResponse));
+        if(yelpResponse != null)
+        {
+            httpContent = new StringContent(
+               JsonConvert.SerializeObject(yelpResponse)
+            );
+        }
+        else {
+                httpContent = new StringContent(
+                    JsonConvert.SerializeObject(this._businesses[0])
+                );
+        }
+
         HttpResponseMessage result = new HttpResponseMessage
         {
             Content = httpContent, 
@@ -73,20 +78,6 @@ public class YelpServiceTests
             .ReturnsAsync(result)
             .Verifiable();
 
-                    // Arrange
-        // var expectedResponse = new YelpResponse()
-        // {
-        //     Businesses = _businesses
-        // };
-
-        // var mockHttpClient = new Mock<HttpClient>();
-        // mockHttpClient.Setup(client => client.GetAsync(It.IsAny<string>()))
-        //               .ReturnsAsync(new HttpResponseMessage
-        //               {
-        //                   StatusCode = HttpStatusCode.OK,
-        //                   Content = new StringContent(JsonConvert.SerializeObject(expectedResponse))
-        //               });
-
         var httpClient = new HttpClient(handlerMock.Object) {
                 BaseAddress = new Uri("https://api.yelp.com/v3")
             };
@@ -97,16 +88,55 @@ public class YelpServiceTests
             
     }
 
+    /// <summary>
+    /// Creates a response object that contains multiple businesses
+    /// </summary>
+    private YelpResponse CreateYelpResponse()
+    {
+        var response = new YelpResponse()
+        {
+            Businesses = _businesses,
+            Reviews = new()
+        };
+
+        return response;
+    }
+
+    /// <summary>
+    /// Creates a single business object
+    /// </summary>
     private void CreateBusinesses()
     {
+        _businesses = new List<Business>();
+        
         for (var i = 0; i < 3; i++)
             _businesses.Add(new()
             {
                 Id = i.ToString(),
                 Name = "business #" + i,
                 Rating = i,
-                Price = "$25.99"
-            });   
+                Price = "$" + (i + 1) + "0.00",
+                Phone = "(555) 555 - 5555",
+                Location = new Location() { City = "Testing", State = "#" + i}
+            });  
+    }
+
+    /// <summary>
+    /// Creates a single business object
+    /// </summary>
+    private void CreateReviews()
+    {
+        _reviews = new List<Review>();
+        
+        for (var i = 0; i < 3; i++)
+            _reviews.Add(new()
+            {
+                Id = i.ToString(),
+                Name = "business #" + i,
+                Rating = i,
+                TimeCreated = DateTime.Now,
+                User = new()
+            });  
     }
 
     private YelpService CreateYelpService()
@@ -116,22 +146,91 @@ public class YelpServiceTests
     }
 
     [Test]
-    public void GetBusinessById()
+    public void CreateClient_StateUnderTest_Valid()
     {
+        CreateMockHttpClient();
         var service = CreateYelpService();
-        var result = service.GetBusinessById("1");
-        
-        string id = result.Result.Data.Id;
+        var result = service.CreateClient();
 
-        Assert.AreEqual("1", id);
+        Assert.That(result, Is.InstanceOf<HttpClient>());
     }
 
     [Test]
-    public void GetBusinessByPhone()
+    public void GetBusinessById_Valid()
     {
-        var service = CreateYelpService();
-        var result = service.GetBusinessByPhone("555 555 5555");
+        CreateMockHttpClient();
 
-        Assert.Equals("SomeName", result.Result.Data.Name);
+        var service = CreateYelpService();
+        var response = service.GetBusinessById("0");
+        
+        string id = response.Result.Data.Id;
+
+        Assert.That(id, Is.EqualTo("0"));
+        Assert.That(response.Result.IsSuccess.Equals(true));
+    }
+                    
+    [Test]
+    public void GetBusinessByPhone_Valid()
+    {
+        var yelpResponse = CreateYelpResponse();
+        CreateMockHttpClient(yelpResponse);
+        
+        var service = CreateYelpService();
+        var response = service.GetBusinessByPhone("(555) 555 - 5555");
+
+        Assert.That(response.Result.Data.Phone, Is.EqualTo("(555) 555 - 5555"));
+        Assert.That(response.Result.IsSuccess.Equals(true));
+    }
+
+    [Test]
+    public void GetBusinessesByLocation_Valid()
+    {
+        var yelpResponse = CreateYelpResponse();
+        CreateMockHttpClient(yelpResponse);
+
+        var service = CreateYelpService();
+        var response = service.GetBusinessesByLocation("Some Location");
+        var lastBusiness = response.Result.Data.Last();
+
+        Assert.That(response.Result.IsSuccess.Equals(true));
+        Assert.That(response.Result.Data.Count, Is.EqualTo(3));
+        Assert.That(lastBusiness.Location.City + " " + lastBusiness.Location.State, Is.EqualTo("Testing #2" ));
+    }
+
+    [Test]
+    public void GetBusinesses_Valid()
+    {
+        var yelpResponse = CreateYelpResponse();
+        CreateMockHttpClient(yelpResponse);
+
+        var service = CreateYelpService();
+        var searchDto = new SearchDto() 
+        {
+            Location = "Test Location",
+            Lat = "0",
+            Long = "0",
+            Terms = new() { "test", "term"},        
+        };
+        var response = service.GetBusinesses(searchDto);
+        var lastBusiness = response.Result.Data.Last();
+        
+        Assert.That(response.Result.IsSuccess.Equals(true));
+        Assert.That(response.Result.Data.Count, Is.EqualTo(3));
+        Assert.That(lastBusiness.Location.City + " " + lastBusiness.Location.State, Is.EqualTo("Testing #2" ));
+    }
+
+    [Test]
+    public void GetReviewsById_Valid()
+    {
+        var yelpResponse = CreateYelpResponse();
+        CreateMockHttpClient(yelpResponse);
+
+        var service = CreateYelpService();
+
+        var response = service.GetReviewsById("2");
+        var lastBusiness = response.Result.Data.Last();
+        
+        Assert.That(response.Result.IsSuccess.Equals(true));
+        Assert.That(response.Result.Data.Count, Is.EqualTo(3));
     }
 }
