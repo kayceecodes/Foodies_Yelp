d18a0cea430970fcf31f04b2561283788487175d
Keith <kayceecode@gmail.com>
2024-10-17 16:29:23 -0400

    edited BusinessProfile and GetBusinessResponse model.

--- Models/Dtos/Responses/GetBusinessResponse.cs
+++ Models/Dtos/Responses/GetBusinessResponse.cs
@@ -3,19 +3,20 @@
 namespace foodies_yelp;
 
 public class GetBusinessResponse
+
 {
     public string Id { get; set; }
-    public string Alias { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public string URL { get; set; } = string.Empty;
-    public int ReviewCount { get; set; }
-    public List<Review> Reviews { get; set; }
-    public List<Category> Categories { get; set; }
+    public string Alias { get; set; }
+    public string Name { get; set; }
+    public string URL { get; set; }
     public int Rating { get; set; }
-    public Coordinates Coordinates { get; set; }
+    public int ReviewCount { get; set; }
+    public List<string> Categories { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
     public string StreetAddress { get; set; }
     public string City { get; set; }
     public string State { get; set; }
     public string ZipCode { get; set; }
-    public string Price { get; set; } = string.Empty;
+    public string Price { get; set; }
 }
--- Profiles/BusinessProfile.cs
+++ Profiles/BusinessProfile.cs
@@ -9,15 +9,18 @@ public class BusinessProfile : Profile
     {
         CreateMap<Business, GetBusinessResponse>()
         .ForMember(dest => dest.Id, src => src.MapFrom(x => x.Id))
+        .ForMember(dest => dest.Alias, src => src.MapFrom(x => x.Alias))
+        .ForMember(dest => dest.Name, src => src.MapFrom(x => x.Name))
+        .ForMember(dest => dest.URL, src => src.MapFrom(x => x.Url))
+        .ForMember(dest => dest.Rating, src => src.MapFrom(x => x.Rating))
+        .ForMember(dest => dest.ReviewCount, src => src.MapFrom(x => x.Review_count))
+        .ForMember(dest => dest.Categories, src => src.MapFrom(x => x.Categories))
+        .ForMember(dest => dest.Latitude, src => src.MapFrom(x => x.Coordinates.Latitude))
+        .ForMember(dest => dest.Longitude, src => src.MapFrom(x => x.Coordinates.Longitude))
         .ForMember(dest => dest.StreetAddress, src => src.MapFrom(x => x.Location.Address1))
         .ForMember(dest => dest.City, src => src.MapFrom(x => x.Location.City))
         .ForMember(dest => dest.State, src => src.MapFrom(x => x.Location.State))
         .ForMember(dest => dest.ZipCode, src => src.MapFrom(x => x.Location.Zip_code))
-        .ForMember(dest => dest.Name, src => src.MapFrom(x => x.Name))
-        .ForMember(dest => dest.Alias, src => src.MapFrom(x => x.Alias))
-        .ForMember(dest => dest.URL, src => src.MapFrom(x => x.Url))
-        .ForMember(dest => dest.ReviewCount, src => src.MapFrom(x => x.Review_count))
-        .ForMember(dest => dest.Categories, src => src.MapFrom(x => x.Categories))
-        .ForMember(dest => dest.Coordinates, src => src.MapFrom(x => x.Coordinates));
+        .ForMember(dest => dest.Price, src => src.MapFrom(x => x.Price));
     }
 }
