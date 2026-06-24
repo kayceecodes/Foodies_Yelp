b14edcfd60d81a3e6a5625bde5da5ee3931c0fed
Keith <kayceecode@gmail.com>
2026-06-24 19:52:55 -0400

    searchdto, searhrequest, & yelpquery objects added

--- Models/Dtos/Requests/SearchDto.cs
+++ Models/Dtos/Requests/SearchDto.cs
@@ -4,20 +4,19 @@ namespace foodies_yelp.Models.Dtos.Requests;
 
 public class SearchDto
 {
-    [Required]
-    public string? Category { get; set; }
 
-    [Required]
-    public double Lat { get; set; }
+    [Required, Range(0, 90)]
+    public double Latitude { get; set; }
 
-    [Required]
-    public double Long { get; set; }
+    [Required, Range(0, 180)]
+    public double Longitude { get; set; }
     
-    [Range(0, 8047)]
-    public int? RadiusMeters { get; set; } // Yelp API has its own default range, it's business-population based
+    [Range(0, 8047)] // (Rounded up) (5 miles == 8046.72)
+    public double? RadiusMeters { get; set; } // Yelp Fusion API has its own default range, it's business-population based. Yelp Fusion Api takes an Integer
 
-    [Required]
     public List<string>? Keywords { get; set; } 
+
+    public int Offset { get; set; }
 
     public int Limit { get; set; }
 }
