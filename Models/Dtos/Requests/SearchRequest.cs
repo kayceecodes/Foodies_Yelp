cecb29bdf4bd44514c19d79fffc1f1c00013a565
Keith <kayceecode@gmail.com>
2026-07-02 19:15:03 -0400

    SearchRequests for YelpService.cs

---
+++ Models/Dtos/Requests/SearchRequest.cs
@@ -0,0 +1,11 @@
+  //var query = $"?sort_by=best_match&limit={dto.Limit}&term={terms}&location={dto.Location}&latitude={dto.Lat}&longitude={dto.Long}";
+
+public class SearchRequest 
+{
+    public List<string> Terms { get; set; }
+    public string Location { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+    public string Offset { get; set; }
+    public string Limit { get; set; }
+}    
